14b40250446e0249f1523e1385a2366a27b393c2
Carlos Daniel Cuellar <34011338+fleadan@users.noreply.github.com>
2021-05-11 03:09:06 -0500

    frewqwer

--- app/src/main/java/com/example/comicstore/ComicCarrito.kt
+++
@@ -1,3 +0,0 @@
-package com.example.comicstore
-
-data class ComicCarrito()
--- app/src/main/java/com/example/comicstore/ComicsAPIService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.comicstore
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface APIService {
-    @GET("comics?apikey=9c40ff86cfd00882d81233dc85a10052&ts=0&hash=3460e37ce0f42aaab22ea73f3f361740")
-    suspend fun getComics() : Response<Results>
-}
